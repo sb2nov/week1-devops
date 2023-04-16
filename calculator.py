ff2332cb1145c201a4a9a0084f727be1369dfc66
vj-m <abijithvijay95@gmail.com>
2023-04-16 17:08:39 -0400

    initial app setup

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Demo Calculator App For Week 1 Project
+
+# Declare variable and set default values
+a = 4
+b = 2
+
+# Addition Method
+def add(a, b):
+    return (a + b)
+
+# Subtract Method
+def sub(a, b):
+    return (a - b)
+
+# Multiplication
+def mul(a, b):
+    return (a * b)
+
+# Division
+def div(a,b):
+    return (a/b)
