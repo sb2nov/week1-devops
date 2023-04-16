5fcc6d969153e442026a1ebc90aa092f2f635400
vj-m <abijithvijay95@gmail.com>
2023-04-16 17:33:00 -0400

    initial app setup changes

--- calculator.py
+++ calculator.py
@@ -1,9 +1,5 @@
 # Demo Calculator App For Week 1 Project
 
-# Declare variable and set default values
-a = 4
-b = 2
-
 # Addition Method
 def add(a, b):
     return (a + b)
@@ -19,3 +15,13 @@ def mul(a, b):
 # Division
 def div(a,b):
     return (a/b)
+
+if __name__ == "__main__":
+    # Declare variable and set default values
+    a = 4
+    b = 2
+
+    print('Sum of ' + str(a) + ' and ' + str(b) + ' is ', add(a, b))
+    print('Difference of ' + str(a) + ' and ' + str(b) + ' is ', sub(a, b))
+    print('Product of ' + str(a) + ' and ' + str(b) + ' is ', mul(a, b))
+    print('Division of ' + str(a) + ' and ' + str(b) + ' is ', div(a, b))
