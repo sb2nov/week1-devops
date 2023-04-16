d01fba13561be2de7342c2ef12b4b7b0b0c6b5e6
vj-m <abijithvijay95@gmail.com>
2023-04-16 17:55:08 -0400

    added unit tests

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 # Demo Calculator App For Week 1 Project
-
 # Addition Method
 def add(a, b):
     return (a + b)
@@ -16,11 +15,22 @@ def mul(a, b):
 def div(a,b):
     return (a/b)
 
+def test_add():
+    assert add(1,1) == 2
+
+def test_sub():
+    assert sub(1,1) == 0
+
+def test_mul():
+    assert mul(1,1) == 1
+
+def test_div():
+    assert div(2,1) == 2
+
 if __name__ == "__main__":
     # Declare variable and set default values
     a = 4
     b = 2
-
     print('Sum of ' + str(a) + ' and ' + str(b) + ' is ', add(a, b))
     print('Difference of ' + str(a) + ' and ' + str(b) + ' is ', sub(a, b))
     print('Product of ' + str(a) + ' and ' + str(b) + ' is ', mul(a, b))
