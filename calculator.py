5fb5fef2689399b4bdccb5e12506e6759172c39e
vj-m <abijithvijay95@gmail.com>
2023-04-24 04:11:34 -0400

    inital code

--- calculator.py
+++ calculator.py
@@ -15,17 +15,6 @@ def mul(a, b):
 def div(a,b):
     return (a/b)
 
-def test_add():
-    assert add(1,1) == 2
-
-def test_sub():
-    assert sub(1,1) == 0
-
-def test_mul():
-    assert mul(1,1) == 1
-
-def test_div():
-    assert div(2,1) == 2
 
 if __name__ == "__main__":
     # Declare variable and set default values
