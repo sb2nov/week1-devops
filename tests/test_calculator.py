89468a0a435744eccf1114600353f2fe26193d5f
Vlad Mocanu <vtmocanu@gmail.com>
2023-06-12 16:33:22 +0300

    fix code

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,10 @@
 from calculator import add, div, mul, sub
 
 
+ def test_add():
+     assert add(1, 1) == 2
 # def test_add():
-#     assert add(1, 1) == 2
-def test_add():
-    assert add(1, 1) == 0
+#     assert add(1, 1) == 0
 
 
 def test_sub():
