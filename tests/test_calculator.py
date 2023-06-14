dc23239b08902b83006242abd054603334b3bed0
Ishan Mittal <51046271+Ishan-Mittal@users.noreply.github.com>
2023-06-14 14:23:38 +0000

    changed test_add to fail

---
+++ tests/test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, sub, mul, div
+
+def test_add():
+    assert add(1, 1) == 0
+
+def test_sub():
+    assert sub(1, 1) == 0
+
+def test_mul():
+    assert mul(5, 1) == 5
+
+def test_div():
+    assert div(6, 2) == 3
