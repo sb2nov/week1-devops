d7927c70b2564a2b47f8d25009ab2d34b7cd49a3
Emmy-code-dev <emmyronald@gmail.com>
2023-06-18 13:11:22 +0100

    Edited test_calcultaor file, created yml file

---
+++ tests/test_calculator.py
@@ -0,0 +1,17 @@
+from calculator import sub, add, mul, div
+
+
+def test_add():
+    assert add(1, 1) == 0
+
+
+def test_sub():
+    assert sub(1, 1) == 0
+
+
+def test_mul():
+    assert mul(1, 1) == 1
+
+
+def test_div():
+    assert div(2, 1) == 2
