09dbe76c3fc7ec126fea2db0ab61e87c51b8dffa
Joy Achieng <odhiambojoy713@gmail.com>
2023-06-18 21:27:49 +0300

    Create workflow

---
+++ tests/test_calculator.py
@@ -0,0 +1,17 @@
+from calculator import add, div, mul, sub
+
+
+def test_add():
+    assert add(1, 1) == 2
+
+
+def test_sub():
+    assert sub(1, 1) == 0
+
+
+def test_mul():
+    assert mul(1, 1) == 1
+
+
+def test_div():
+    assert div(2, 1) == 2
