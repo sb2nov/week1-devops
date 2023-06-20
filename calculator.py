cf5e9171f8a2535ef8cd85e0da67595421c39007
Vishal Waghmode <vishalwaghmode007@gmail.com>
2023-06-20 14:35:48 +0000

    changed test_add to fail

--- calculator.py
+++ calculator.py
@@ -27,3 +27,5 @@ if __name__ == "__main__":
     print("Difference of " + str(a) + " and " + str(b) + " is ", sub(a, b))
     print("Product of " + str(a) + " and " + str(b) + " is ", mul(a, b))
     print("Division of " + str(a) + " and " + str(b) + " is ", div(a, b))
+
+
